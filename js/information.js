884168cbc1e9d42cae2de327338a54d3ab1da678
nocoolyoyo <574857444@qq.com>
2016-09-26 21:34:26 +0800

    datepicker

--- js/information.js
+++ js/information.js
@@ -1,3 +1,19 @@
-/**
- * Created by dell-pc on 2016/9/26.
- */
+(function() {
+    $(function(){
+        /*
+         *  功能：日期悬着器API
+         *  页面：*.html
+         *  Created by nocoolyoyo 2016/9/26.
+         */
+        $('.form_date').datetimepicker({
+            language:  'zh-CN',
+            weekStart: 1,
+            todayBtn:  1,
+            autoclose: 1,
+            todayHighlight: 1,
+            startView: 2,
+            minView: 2,
+            forceParse: 0
+        });
+    });
+}());
