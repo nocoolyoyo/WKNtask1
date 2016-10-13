fc5b77a193948a9a217e189bee934eee8bb60b77
nocoolyoyo <574857444@qq.com>
2016-10-14 01:01:45 +0800

    SMS-add add

--- js/SMS.js
+++ js/SMS.js
@@ -6,7 +6,7 @@
 
 
         $(document).on("click", "#add", function() {
-            initSMSCreate();
+            initSMSAdd();
         });
 
         $(document).on("click", "#reply", function() {
@@ -19,8 +19,124 @@
             initTimepicker();
         });
 
-        function initSMSCreate(){
-                    //d等待添加
+        function initSMSAdd(){
+            $container = $("#main-box");
+            $.ajax({
+                url: "./data/SMS-add.html",
+                async: false,
+                contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+                success: function (data) {
+                    $container.html(data);
+                }
+            });
+            initTableMembers();
+            function initTableMembers(){
+                var $tableMembers = $('#table');
+                $add = $('#add');
+                $tableMembers.bootstrapTable({
+                    url: 'data/occupation.json',
+                    idField: "id",
+                    pageSize: 4,
+                    pageList: 'all',
+                    paginationVAlign: top,
+                    sidePagination: 'client',
+                    pagination: true,
+                    toolbar: "#table-toolbar",
+                    // sidePagination: "server",
+                    showColumns: true,
+                    height: 300,
+                    columns: [{
+                        field: 'state',
+                        checkbox: true
+
+                    }, {
+                        field: 'id',
+                        title: 'ID',
+                        sortable: true,
+                        align: 'center'
+                    }, {
+                        field: 'REALNAME',
+                        title: '姓名',
+                        sortable: true,
+                        align: 'center'
+                    }, {
+                        field: 'MOBILE',
+                        title: '手机号',
+                        sortable: true,
+                        align: 'center'
+                    }, {
+                        field: 'COMPANY',
+                        title: '所在单位',
+                        sortable: true,
+                        align: 'center'
+                    }, {
+                        field: 'ONAME',
+                        title: '单位职务',
+                        sortable: true,
+                        align: 'center'
+                    }, {
+                        field: 'GRADE',
+                        title: '单位职务',
+                        sortable: true,
+                        align: 'center'
+                    }]
+                });
+
+                /*
+                 *  功能：获取选择框信息
+                 *  Created by nocoolyoyo 2016/9/28.
+                 */
+
+                $tableMembers.on('check.bs.table uncheck.bs.table ' +
+                    'check-all.bs.table uncheck-all.bs.table', function () {
+                    if ($tableMembers.bootstrapTable('getSelections').length) {
+                        $add.show();
+                    } else {
+                        $add.hide();
+                    }
+                    selections = getIdSelections();
+                });
+                $add.click(function () {
+                    var ids = getIdSelections();
+                    $tableMembers.bootstrapTable('remove', {
+                        field: 'id',
+                        values: ids
+                    });
+                    $add.hide();
+                });
+                function getIdSelections() {
+                    return $.map($tableMembers.bootstrapTable('getSelections'), function (row) {
+                        return row.id
+                    });
+                }
+            }
+            initMembersSelected();
+            function initMembersSelected(){
+                var $tableSelected = $('#members-selected');
+
+                $tableSelected.bootstrapTable({
+                    url: 'data/occupation.json',
+                    idField: "id",
+                    pageNumber: 12,
+                    height: 590,
+                    search: true,
+                    showHeader:false,
+                    toolbar: "#left-toolbar",
+
+                    columns: [{
+                        field: 'REALNAME',
+                        title: '姓名',
+                        sortable: true,
+                        align: 'center'
+                    }, {
+                        field: 'MOBILE',
+                        title: '手机号',
+                        sortable: true,
+                        align: 'center'
+                    }]
+                });
+
+            }
         }
 
 
@@ -34,7 +150,7 @@
                     $container.html(data);
                 }
             });
-            initTable2();
+          initTableMembers();
         }
 
         function initSMSIndex(){
